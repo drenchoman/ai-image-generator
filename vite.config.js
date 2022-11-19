a7f56623825ea0a0da2fd9f5fd41f642f7bfb8b8
Oscar Harron <oscarharron@hotmail.com>
2022-11-19 14:52:27 +1300

    prod env

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.VITE_OPEN_AI_KEY': `"${process.env.VITE_OPEN_AI_KEY}"`,
+    'process.env': process.env,
   },
 })
