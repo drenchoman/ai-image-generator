b0d190fd747008d87f0af3b664b710289e89ad25
Oscar Harron <oscarharron@hotmail.com>
2022-11-19 13:13:08 +1300

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_OPEN_AI_KEY': `"${process.env.VITE_OPEN_AI_KEY}"`,
+  },
 })
